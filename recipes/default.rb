c58e88d5f1d6c0490493dfa7fb7edb1de59c6c2f
Nathen Harvey <nharvey@customink.com>
2012-07-26 07:33:49 -0400

    Cleaning up whitespace in the recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,54 +22,53 @@
 
 unless node['platform'] == "redhat" and  node['platform_version'].to_i  != 6
 
-package "libcgroup" do
-  action :install
-end
+  package "libcgroup" do
+    action :install
+  end
 
 
 
-template "/etc/cgconfig.conf" do
-   source "cgconfig.conf.erb"
-   variables(
-    :controllers => node['cgroups']['controllers'],
-    :parameters => node['cgroups']['parameters']
-      )  
-notifies :restart, "service[cgconfig]"
-notifies :restart, "service[cgred]"
+  template "/etc/cgconfig.conf" do
+    source "cgconfig.conf.erb"
+    variables(
+      :controllers => node['cgroups']['controllers'],
+      :parameters => node['cgroups']['parameters']
+    )
+    notifies :restart, "service[cgconfig]"
+    notifies :restart, "service[cgred]"
 
-if ::File.exists? "/etc/init.d/#{node['cgroups']['service']}"
-notifies :restart, "service[#{node['cgroups']['service']}]"
-end
-end
-
-
-
-template "/etc/cgrules.conf" do
-  source "cgrules.conf.erb"
-  variables(
-    :users => node['cgroups']['users']
-)
-notifies :restart, "service[cgred]"
-notifies :restart, "service[cgconfig]"
-
-if ::File.exists? "/etc/init.d/#{node['cgroups']['service']}"
-notifies :restart, "service[#{node['cgroups']['service']}]"
-end
-end
+    if ::File.exists? "/etc/init.d/#{node['cgroups']['service']}"
+      notifies :restart, "service[#{node['cgroups']['service']}]"
+    end
+  end
 
 
 
-service node['cgroups']['service']  do
-only_if {::File.exists? "/etc/init.d/#{node['cgroups']['service']}"}
-end
+  template "/etc/cgrules.conf" do
+    source "cgrules.conf.erb"
+    variables(
+      :users => node['cgroups']['users']
+    )
+    notifies :restart, "service[cgred]"
+    notifies :restart, "service[cgconfig]"
+
+    if ::File.exists? "/etc/init.d/#{node['cgroups']['service']}"
+      notifies :restart, "service[#{node['cgroups']['service']}]"
+    end
+  end
 
 
-service "cgconfig" do
-  action [:enable]
-end
 
-service "cgred" do
-  action [:enable]
-end
+  service node['cgroups']['service']  do
+    only_if {::File.exists? "/etc/init.d/#{node['cgroups']['service']}"}
+  end
 
+
+  service "cgconfig" do
+    action [:enable]
+  end
+
+  service "cgred" do
+    action [:enable]
+  end
 end
